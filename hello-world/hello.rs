2133d9344c64bf4fe1512e1be1a53d0b55496a24
fibo <fibo@users.noreply.github.com>
2022-02-26 09:46:24 +0100

    hello Rust

---
+++ hello-world/hello.rs
@@ -0,0 +1,15 @@
+// Code from https://doc.rust-lang.org/rust-by-example/hello.html
+
+// This is the main function
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // println! is a macro that prints text to the console.
+    println!("Hello World!");
+}
+
+// Compile it with
+//     rustc hello.rs
+
+// Then run it with
+//     ./hello`
